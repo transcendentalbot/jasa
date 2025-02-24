286effe7451b6eb4d95b37f403eed8058187e72f
Jagadeesh Ranganathan <jranganathan@pc.factset.com>
2025-02-23 21:04:35 -0500

    try basic api

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
-module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: ["babel-preset-expo"],
-      plugins: ["expo-router/babel"], // ✅ Keep only Expo Router if you're using it
-    };
-  };
-  
+module.exports = {
+  presets: ["babel-preset-expo"],
+};
